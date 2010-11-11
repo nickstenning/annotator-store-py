c8700900ee9ad7b395cb3ff9c66181daa035a38a
rgrp <http://rufuspollock.org>
2010-11-11 09:18:24 +0000

    [docs][xs]: update version number and main docstring.

--- annotator/__init__.py
+++ annotator/__init__.py
@@ -1,7 +1,7 @@
-"""Annotation of a web resource.
+"""Backend(s) for web annotation.
 
 @copyright: (c) 2006-2010 Open Knowledge Foundation
 """
-__version__ = '0.4a'
+__version__ = '0.4'
 __license__ = 'MIT'
 __author__ = 'Rufus Pollock (Open Knowledge Foundation)'
